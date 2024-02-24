02e6f15fd78b9d990a590ca24c9abc12d86df656
Derrick Henderson <2019dwh@gmail.com>
2024-02-23 18:24:42 -0600

    Output result, comments made for future reference

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// Define function greet
 function greet(name){
     return "Hello, " + name
 }
 
+// Call the function greet
 const greeting = greet("Derrick")
+
+// Print the result to the console
+console.log(greeting);
