a6ec35dec2cc9a71839db6d8eaa860142d7ef2ae
Derrick Henderson <2019dwh@gmail.com>
2024-03-18 21:43:45 -0500

    Create and call function verifyAdulthood

--- tuesday.js
+++ tuesday.js
@@ -1,2 +1,12 @@
 const name = `Derrick`;
 const age = 22;
+
+function verifyAdulthood(name, age) {
+  if (age < 18) {
+    return `You are not old enough to enter, ${name}!`;
+  }
+
+  return `Welcome, ${name}`;
+}
+
+console.log(verifyAdulthood(name, age));
