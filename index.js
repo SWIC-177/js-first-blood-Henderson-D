fbb15105f551da712a8c016d8d0697947c3cb7f4
Derrick Henderson <2019dwh@gmail.com>
2024-03-28 17:26:34 -0500

    Include given code from assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+const people = [
+  {
+    name: "John",
+    age: 17,
+  },
+  {
+    name: "Jane",
+    age: 20,
+  },
+  {
+    name: "Mary",
+    age: 25,
+  },
+  {
+    name: "Peter",
+    age: 30,
+  },
+  {
+    name: "Paul",
+    age: 35,
+  },
+];
+
+function verifyAdulthood(personName, personAge) {
+  if (personAge >= 18) return `Welcome, ${personName}!`;
+
+  return `You are not old enough to enter, ${personName}.`;
+}
