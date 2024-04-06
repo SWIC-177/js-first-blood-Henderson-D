b7219917dfb03f342adf83dd9ab89c083c27757e
John Doe <2019dwh@gmail.com>
2024-04-06 18:57:24 -0500

    Create and finish football score example

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const arrayName = [
-  123
-];
+const message = "The score is: ";
 
-function functionName(array) {
-  return (prefix) => `${prefix} ${array[0]}`;
+while (true) {
+    let totalScore = 0;
+
+    for (let i = 0; i < 70; i++) {
+        const point = parseInt(prompt('What point was scored? '));
+        
+        const newScore = (score) => {
+            if (score % 6 === 0 || score % 3 === 0 || score % 2 === 0) {
+                totalScore += score;
+                return totalScore;
+            } else {
+                return "Not a valid point";
+            } 
+        };
+        
+        const messageScore = (scoreIs, addedScore) => {
+            return `${scoreIs} ${addedScore}`;
+        };
+
+        console.log(messageScore(message, newScore(point)));
+    }
 }
-
-const variableName = functionName(arrayName);
-console.log(variableName("Prefix"));
