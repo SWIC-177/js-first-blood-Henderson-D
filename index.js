cb8a6fe8dce9e82385052c0df753f51b24366a1b
John Doe <2019dwh@gmail.com>
2024-04-06 18:56:08 -0500

    Create example3, calling functions from functions

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-const todayDate = () => {
-  const currentDate = new Date();
-   
-  const year = currentDate.getFullYear();
-  const month = currentDate.getMonth() + 1;
-  const day = currentDate.getDate();
-  
-  return(`${year}-${month}-${day}`);
-  }
-  
-  const nameAge = (name, age) => {
-    const message = `Hello, ${name}! You are ${age}`
-    return(message)
-  }
-  
-  const customMessage = (greeting, date) => {
-      const welcomeMessage = `${greeting}` + ` and today is ${date}`
-      return welcomeMessage
-  }
-  
-  console.log(customMessage(nameAge('Derrick', 22), todayDate()))
+const arrayName = [
+  123
+];
+
+function functionName(array) {
+  return (prefix) => `${prefix} ${array[0]}`;
+}
+
+const variableName = functionName(arrayName);
+console.log(variableName("Prefix"));
+
+returns //"Prefix" araryName.item
