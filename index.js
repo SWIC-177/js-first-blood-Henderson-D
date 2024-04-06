2d4cb5dec98e2667689f1bb0ce0af81d5684d042
John Doe <2019dwh@gmail.com>
2024-04-06 18:54:41 -0500

    Create example2, passing functions as arguments

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const nameAge = (name, age) => {
-	const message = `Hello, ${name}! You are ${age}`
-	return(message)
-}
-console.log(nameAge('Derrick', 22))
+const todayDate = () => {
+  const currentDate = new Date();
+   
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth() + 1;
+  const day = currentDate.getDate();
+  
+  return(`${year}-${month}-${day}`);
+  }
+  
+  const nameAge = (name, age) => {
+    const message = `Hello, ${name}! You are ${age}`
+    return(message)
+  }
+  
+  const customMessage = (greeting, date) => {
+      const welcomeMessage = `${greeting}` + ` and today is ${date}`
+      return welcomeMessage
+  }
+  
+  console.log(customMessage(nameAge('Derrick', 22), todayDate()))
