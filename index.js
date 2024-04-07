ee6df496d5c92bb9f76241a195f8b067a9ed5a20
John Doe <2019dwh@gmail.com>
2024-04-06 19:15:46 -0500

    Create one last example for closure

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-const message = "The score is: ";
-
-while (true) {
-    let totalScore = 0;
-
-    for (let i = 0; i < 70; i++) {
-        const point = parseInt(prompt('What point was scored? '));
-        
-        const newScore = (score) => {
-            if (score % 6 === 0 || score % 3 === 0 || score % 2 === 0) {
-                totalScore += score;
-                return totalScore;
-            } else {
-                return "Not a valid point";
-            } 
-        };
-        
-        const messageScore = (scoreIs, addedScore) => {
-            return `${scoreIs} ${addedScore}`;
-        };
-
-        console.log(messageScore(message, newScore(point)));
-    }
+const meInfo = {
+    name: 'Derrick',
+    age: 22
 }
+    
+function welcome(person){
+    return (welcome) => `${welcome}, ${person.name}`
+}
+
+const customMessage =  welcome(meInfo)
+
+console.log(customMessage("Hey"))
