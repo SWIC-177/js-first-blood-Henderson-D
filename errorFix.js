9bed31a6be3d267dfac4d25e72867e13148bc0c4
Derrick Henderson <2019dwh@gmail.com>
2024-04-09 18:30:25 -0500

    Correct unary operator and avoid mutations

---
+++ errorFix.js
@@ -0,0 +1,45 @@
+const people = [
+  {
+    id: 1,
+    name: "John Doe",
+    age: 23,
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    age: 29,
+  },
+  {
+    id: 3,
+    name: "Jim Doe",
+    age: 34,
+  },
+  {
+    id: 4,
+    name: "Jill Doe",
+    age: 45,
+  },
+  {
+    id: 5,
+    name: "Jack Doe",
+    age: 56,
+  },
+  {
+    id: 6,
+    name: "Jenny Doe",
+    age: 67,
+  },
+];
+
+const renameOdds = (p) => {
+  const newPeople = { ...p };
+  for (let i = 0; i < p.length; i += 1) {
+    if (p[i].id % 2 !== 0) {
+      p[i].name = "Odd Name";
+    }
+  }
+
+  return p;
+};
+
+console.log(renameOdds(people));
