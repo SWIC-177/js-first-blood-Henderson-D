83479b5e0b73c495858ae03a6a6423dbd464b230
Derrick Henderson <2019dwh@gmail.com>
2024-04-09 18:42:16 -0500

    Call function and passthrough object literal

--- refractor.js
+++ refractor.js
@@ -1,6 +1,12 @@
 const cleverName = {
   name: "Derrick",
-  age: 23,
+  age: 22,
 };
 
-const functionName = (list) => {};
+const functionName = (list) => ({
+  ...list,
+  job: "Business Software Implementation/Development",
+  age: 22.89,
+});
+
+console.log(functionName(cleverName));
